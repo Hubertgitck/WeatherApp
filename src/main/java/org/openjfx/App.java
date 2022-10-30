4dfb5ed03091b34d9e3f843a9e517dcf336b8ddc
Hubertgitck <103528563+Hubertgitck@users.noreply.github.com>
2022-10-30 14:35:10 +0100

    Setting up project core.

---
+++ src/main/java/org/openjfx/App.java
@@ -0,0 +1,24 @@
+package org.openjfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
